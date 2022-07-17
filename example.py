d072f948ae21c018e1355dfd509354f6cf4d4a8c
Val Knight <val@valknight.xyz>
2022-07-17 16:56:56 +0100

    Move main to example!

---
+++ example.py
@@ -0,0 +1,27 @@
+from cohost.models.user import User
+from cohost.models.post import Post
+from cohost.models.block import AttachmentBlock, MarkdownBlock
+from cohost.models.project import Project, EditableProject
+import os
+import json
+def main():
+    cookie = os.environ.get('COHOST_COOKIE')
+    if cookie is None:
+        print('COHOST_COOKIE environment variable not set - please paste your cookie below')
+        print('To skip this, please set the COHOST_COOKIE environment variable to the cookie you want to use')
+        cookie = input('COHOST_COOKIE: ')
+    user = User.loginWithCookie(cookie)
+    project = user.getProject('yourhandle')
+    print('Logged in as: {}'.format(project))
+    blocks = [
+        MarkdownBlock('hello from cohost!'),
+        AttachmentBlock('pybug.png'),
+    ]
+    p = project.post('Cohost.py is working!',
+                     blocks,
+                     adult=False, draft=False, tags=['cohost.py', 'python'])
+    print('Live at: {}'.format(p.url))
+    
+
+if __name__ == '__main__':
+    main()
